80d85054f11df225de3a9c0fa2cfd8205955b3b8
Carl Wood <cjwood17@googlemail.com>
2017-02-22 22:09:06 +0000

    Add mins played and player position

--- src/components/player.js
+++ src/components/player.js
@@ -11,7 +11,7 @@ class Player extends React.Component {
 class PlayerIntro extends React.Component {
     render() {
         return (
-            <div className="form-intro"> 
+            <div className="form-intro">
                 <p>Calculate your player's potential points by completing this form.</p>
             </div>
         )
@@ -24,27 +24,36 @@ class PlayerForm extends React.Component {
         super(props);
         this.state = {
             name: null,
+            mins: 0,
             goals: null,
-            assists: null
+            assists: null,
+            position: 'gk'
         };
         this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePositionChange = this.handlePositionChange.bind(this);
+        this.handleMinsChange = this.handleMinsChange.bind(this);
         this.handleGoalsChange = this.handleGoalsChange.bind(this);
         this.handleAssistsChange = this.handleAssistsChange.bind(this);
     }
 
     handleNameChange(event) {
         this.setState({name: event.target.value});
-        console.log(this.state.name);
+    }
+
+    handlePositionChange(event) {
+        this.setState({position: event.target.value});
+    }
+
+    handleMinsChange(event) {
+        this.setState({mins: event.target.value});
     }
 
     handleGoalsChange(event) {
         this.setState({goals: event.target.value});
-        console.log(this.state);
     }
 
     handleAssistsChange(event) {
         this.setState({assists: event.target.value});
-        console.log(this.state.assists);
     }
 
     render() {
@@ -58,6 +67,19 @@ class PlayerForm extends React.Component {
                             <input className="form-input" type="text" onChange={this.handleNameChange} />
                         </div>
                         <div className="form-group">
+                            <label className="form-label">Position</label>
+                            <select className="form-input" onChange={this.handlePositionChange}>
+                                <option value="gk">GK</option>
+                                <option value="def">DEF</option>
+                                <option value="mid">MID</option>
+                                <option value="fwd">FWD</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label className="form-label">Minutes Played</label>
+                            <input className="form-input" type="range" max="90" onChange={this.handleMinsChange} />
+                        </div>
+                        <div className="form-group">
                             <label className="form-label">Goals</label>
                             <input className="form-input" type="number" onChange={this.handleGoalsChange} />
                         </div>
@@ -69,10 +91,13 @@ class PlayerForm extends React.Component {
                     </form>
                 </div>
                 <div className="player-result">
-                    <PlayerResult 
-                        name={this.state.name} 
+                    <PlayerResult
+                        name={this.state.name}
+                        pos={this.state.position}
+                        mins={this.state.mins}
                         goals={this.state.goals} 
-                        assists={this.state.assists}/>
+                        assists={this.state.assists}
+                    />
                 </div>
             </div>
         );
@@ -87,14 +112,21 @@ class PlayerResult extends React.Component {
         )
     }
 
-    render() {
+    renderAssistPoints() {
+        return (
+            <span>{this.props.assists * 4}</span>
+        )
+    }
 
+    render() {
+        const minsPoints = this.props.mins > 60 ? 2 : 1;
         return (
             <div>
                 <div className="player-name">{this.props.name}</div>
-                <div><b>78</b> minutes played (2)</div>
+                <div>{this.props.pos}</div>
+                <div><b>{this.props.mins}</b> minutes played ({minsPoints})</div>
                 <div><b>{this.props.goals}</b> goals ({this.renderGoalPoints()})</div>
-                <div><b>{this.props.assists}</b> assist (5)</div>
+                <div><b>{this.props.assists}</b> assist ({this.renderAssistPoints()})</div>
                 <span className="player-points">17</span>
                 <span className="player-points-label">points</span>
             </div>
